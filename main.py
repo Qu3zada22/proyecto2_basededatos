ab807de58a39341407105c3c919d6f07529c1254
DavidDominguez-11 <dominguezdavid396@gmail.com>
2025-04-09 19:45:11 -0600

    re-strctured project

--- main.py
+++
@@ -1,7 +0,0 @@
-# main.py
-from simulador import simular_usuarios
-
-if __name__ == "__main__":
-    for cantidad in [5, 10, 20, 30]:
-        print(f"\n>>> Simulando con {cantidad} usuarios simultáneos:\n")
-        simular_usuarios(cantidad)
